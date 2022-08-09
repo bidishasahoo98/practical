230309c1a3d4325fd3a747b206147e8262612a8c
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-09 16:43:06 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,11 @@
+class A{
+static int c=50;
+int a=20;
+public static void main (String[]args){
+A ob=new A();
+int b=25;
+System.out.println(A.c);
+System.out.println(b);
+System.out.println(ob.a);
+}
+}
