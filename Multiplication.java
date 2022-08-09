a07765916ed16fe12d8838822578c002c2cbe81a
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-09 17:36:26 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+class Multiplication{
+public static void main(String []args){
+System.out.println("two numbers are: ");
+int a=6; // initialize 1st local variable
+int b=5; // initialize 2nd local variable
+System.out.println(a+"  " +b);
+int product= a*b;
+System.out.println("product is : " +product);
+}
+}
