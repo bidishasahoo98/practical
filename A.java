0e0b0b04020595a874fe090cb3c8f67502a9e961
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-09 16:49:35 +0530

    Update A.java

--- A.java
+++ A.java
@@ -1,9 +1,9 @@
 class A{
-static int c=50;
-int a=20;
+static int c=50;//SV
+int a=20;//IV
 public static void main (String[]args){
 A ob=new A();
-int b=25;
+int b=25;//LC
 System.out.println(A.c);
 System.out.println(b);
 System.out.println(ob.a);
