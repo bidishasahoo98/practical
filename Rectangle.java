2c0c8795d61042939e7bb260debada605e05a387
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-10 15:28:00 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle{
+public static void main(String[]args){
+int length =4;
+int breadth=2;
+System.out.println("Enter the value of length :" +length);
+System.out.println("Enter the value of breadth :" +breadth);
+int area= (length*breadth);
+int perimeter=2*(length+breadth);
+System.out.println("Area of rectangle is :" +area);
+System.out.println("Perimeter of rectangle is:" +perimeter);
+}
+}
