ffb85272bd005deb3be08c90cfaa9576255092c7
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-16 17:30:47 +0530

    Add files via upload

---
+++ Operatorexample.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class Operatorexample {
+
+	public static void main(String[] args) {
+		// initialize a&b
+		int a=10;
+		int b=20;
+		System.out.println(a++ + b++); //unary + arithmatic operator use
+		System.out.println(a++<b && b++>a);//relational +logical operator use
+}
+}
