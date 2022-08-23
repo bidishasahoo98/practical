65e1b2d5174bd1f82da4b74964034dd78adf2446
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-23 16:40:20 +0530

    Add files via upload

---
+++ Forloopreverse.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class Forloopreverse {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=5;i++) //outer loop
+		{
+			for(int j=5; j>=i;j--)  //inner loop
+			{
+				 System.out.print("*");
+}
+			System.out.println();} //new line
+
+	}
+
+}
