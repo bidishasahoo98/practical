0b4c5b7c86dec3b3d9d2aca0d68ba0b98149ac11
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-23 18:04:35 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=9;i++) //outer loop
+		{
+			for(int j=1; j<=i;j++)  //inner loop
+			{
+				 System.out.print(j);
+}
+			System.out.println();} //new line
+	}
+
+}
