156dda411e9b5fe81afb8c17e3cbedf3689f435e
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-25 15:10:10 +0530

    Add files via upload

---
+++ Dowhileloop.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class Dowhileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=0;
+System.out.println("1st 10 even number");
+do {
+	System.out.println(i);
+	i=i+2;   //exit control loop
+}while(i<=10);  //condition at last
+	}
+
+}
---
+++ Whileloop1.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class Whileloop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while(true) {
+			System.out.println("infinity time of while loop");
+		}
+	}
+
+}
