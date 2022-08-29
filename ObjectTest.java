ec7a9e53be985bc61505add21eeb38d1930860e1
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-29 21:40:14 +0530

    Add files via upload

---
+++ ObjectTest.java
@@ -0,0 +1,19 @@
+package operator;
+		class ObjectTest1{
+			//field
+			int age;
+			String name;
+		}
+//create another class
+		public class ObjectTest {
+//main method
+			public static void main(String[] args) {
+				ObjectTest1 ob=new ObjectTest1(); //creating object
+//print it
+				System.out.println(ob.age);
+				System.out.println(ob.name);
+
+			}
+
+		}
+
