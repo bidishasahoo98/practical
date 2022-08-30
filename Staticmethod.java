9cb4ffb701a3bb5e20b4e2b0dc16b6e3dc25efe5
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-30 16:22:33 +0530

    Add files via upload

---
+++ Staticmethod.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class Staticmethod {
+
+	public static void main(String[] args) {
+		display();
+	}
+	static void display() { //static method
+		System.out.println("Welcome");
+
+	}
+
+}
