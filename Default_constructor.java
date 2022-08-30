1ad6471ae51866c560e6b663c2a39c74167f5a98
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-08-30 17:32:31 +0530

    Add files via upload

---
+++ Default_constructor.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class Default_constructor {
+	Default_constructor(){
+		System.out.println("constructor is created");
+	}
+	public static void main(String[] args) {
+		Default_constructor ob=new Default_constructor();
+	}
+
+}
